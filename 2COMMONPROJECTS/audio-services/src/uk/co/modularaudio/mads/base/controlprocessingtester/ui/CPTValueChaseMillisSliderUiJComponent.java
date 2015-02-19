8fc66ab281aef220dee3a8de9e283d0275a0419e
Daniel Hams <daniel.hams@gmail.com>
2015-02-19 06:31:08 +0000

    The slider and related container allowing the setting of the value fade in millis.

---
+++ 2COMMONPROJECTS/audio-services/src/uk/co/modularaudio/mads/base/controlprocessingtester/ui/CPTValueChaseMillisSliderUiJComponent.java
@@ -0,0 +1,122 @@
+/**
+ *
+ * Copyright (C) 2015 - Daniel Hams, Modular Audio Limited
+ *                      daniel.hams@gmail.com
+ *
+ * Mad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mad.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package uk.co.modularaudio.mads.base.controlprocessingtester.ui;
+
+import java.awt.Color;
+
+import javax.swing.JComponent;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import uk.co.modularaudio.mads.base.controlprocessingtester.mu.CPTMadDefinition;
+import uk.co.modularaudio.mads.base.controlprocessingtester.mu.CPTMadInstance;
+import uk.co.modularaudio.util.audio.gui.mad.IMadUiControlInstance;
+import uk.co.modularaudio.util.audio.mad.ioqueue.ThreadSpecificTemporaryEventStorage;
+import uk.co.modularaudio.util.audio.mad.timing.MadTimingParameters;
+import uk.co.modularaudio.util.swing.mvc.sliderdisplay.SliderDisplayView.DisplayOrientation;
+import uk.co.modularaudio.util.swing.mvc.sliderdisplay.SliderDisplayView.SatelliteOrientation;
+
+public class CPTValueChaseMillisSliderUiJComponent extends TimeSlider
+	implements IMadUiControlInstance<CPTMadDefinition, CPTMadInstance, CPTMadUiInstance>
+{
+	private static Log log = LogFactory.getLog( CPTValueChaseMillisSliderUiJComponent.class.getName() );
+
+	private static final long serialVersionUID = 2538907435465770032L;
+
+	private final CPTMadUiInstance uiInstance;
+
+	public CPTValueChaseMillisSliderUiJComponent( final CPTMadDefinition definition,
+			final CPTMadInstance instance,
+			final CPTMadUiInstance uiInstance,
+			final int controlIndex )
+	{
+		super( 1.0f, 10.0f, 10.0f,
+				"ms",
+				SatelliteOrientation.LEFT,
+				DisplayOrientation.HORIZONTAL,
+				SatelliteOrientation.RIGHT,
+				"Chase:",
+				Color.WHITE,
+				Color.WHITE,
+				false );
+		this.uiInstance = uiInstance;
+	}
+
+	@Override
+	public JComponent getControl()
+	{
+		return this;
+	}
+
+	private void passChangeToInstanceData( final float newValue )
+	{
+		uiInstance.setChaseValueMillis( newValue );
+	}
+
+	@Override
+	public void doDisplayProcessing( final ThreadSpecificTemporaryEventStorage tempEventStorage,
+			final MadTimingParameters timingParameters,
+			final long currentGuiTime)
+	{
+		// log.debug("Received display tick");
+	}
+
+	@Override
+	public void destroy()
+	{
+	}
+
+	@Override
+	public String getControlValue()
+	{
+		return model.getValue() + "";
+	}
+
+	@Override
+	public void receiveControlValue( final String valueStr )
+	{
+		try
+		{
+//			log.debug("Received control value " + value );
+			final float asFloat = Float.parseFloat( valueStr );
+			model.setValue( this, asFloat );
+			receiveValueChange( this, asFloat );
+		}
+		catch( final Exception e )
+		{
+			final String msg = "Failed to parse control value: " + valueStr;
+			log.error( msg, e );
+		}
+	}
+
+	@Override
+	public void receiveValueChange( final Object source, final float newValue )
+	{
+		passChangeToInstanceData( newValue );
+	}
+
+	@Override
+	public boolean needsDisplayProcessing()
+	{
+		return false;
+	}
+}
