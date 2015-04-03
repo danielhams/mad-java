a4849adc58c5ec672a8dfcb8e2a8c52975fca794
Daniel Hams <daniel.hams@gmail.com>
2015-04-03 20:24:23 +0100

    Created some comparative methods to show the benefits of the bulk method (non bulk takes just under twice the time).

--- 4EXTERNAL/external-libsndfile-services-test/src/test/uk/co/modularaudio/libsndfilewrapper/AttemptToReadAFile.java
+++ 4EXTERNAL/external-libsndfile-services-test/src/test/uk/co/modularaudio/libsndfilewrapper/AttemptToReadAFile.java
@@ -17,28 +17,24 @@ public class AttemptToReadAFile
 
 	private final static int BUFFER_LENGTH_FLOATS = 4096;
 
-	private final float[] buffer = new float[ BUFFER_LENGTH_FLOATS ];
+	private final float[] buffer = new float[BUFFER_LENGTH_FLOATS];
+
+	final String filePath = "/home/dan/Music/CanLoseMusic/Albums/Regular/depeche_mode/a_broken_frame/a_photograph_of_you.ogg";
+//	final String filePath = "/home/dan/Music/CanLoseMusic/Albums/Regular/ORB - The Orb's Adventures Beyond The Ultraworld/CD 1/02 - Earth Orbit Two- Earth (Gaia).flac";
+//	final String filePath = "/home/dan/Music/PreferNotToLoseMusic/SetSources/Mp3Repository/200911/974684_She_Came_Along_feat__Kid_Cudi_Sharam_s_Ecstasy_Of_Ibiza_Edit.mp3";
 
 	public AttemptToReadAFile()
 	{
+		LibSndfileWrapperLoader.loadIt();
 	}
 
-	public void go() throws Exception
+	public void goSlowMethod() throws Exception
 	{
-		LibSndfileWrapperLoader.loadIt();
-
 		final SF_INFO sf = new SF_INFO();
-
-		final String filePath = "/home/dan/Music/CanLoseMusic/Albums/Regular/depeche_mode/a_broken_frame/a_photograph_of_you.ogg";
-//		final String filePath = "/home/dan/Music/CanLoseMusic/Albums/Regular/ORB - The Orb's Adventures Beyond The Ultraworld/CD 1/02 - Earth Orbit Two- Earth (Gaia).flac";
-//		final String filePath = "/home/dan/Music/PreferNotToLoseMusic/SetSources/Mp3Repository/200911/974684_She_Came_Along_feat__Kid_Cudi_Sharam_s_Ecstasy_Of_Ibiza_Edit.mp3";
 
 		log.trace( "Beginning" );
 
-		final SWIGTYPE_p_SNDFILE_tag sndfilePtr = libsndfile.sf_open(
-				filePath,
-				libsndfile.SFM_READ,
-				sf );
+		final SWIGTYPE_p_SNDFILE_tag sndfilePtr = libsndfile.sf_open( filePath, libsndfile.SFM_READ, sf );
 
 		if( sndfilePtr == null )
 		{
@@ -48,15 +44,11 @@ public class AttemptToReadAFile
 		final int sampleRate = sf.getSamplerate();
 		final long numFrames = sf.getFrames();
 		final int numChannels = sf.getChannels();
-		log.trace( "Apparently have opened the file with (" +
-				sampleRate + ") (" +
-				numFrames + ") (" +
-				numChannels + ")" );
+		log.trace( "Apparently have opened the file with (" + sampleRate + ") (" + numFrames + ") (" + numChannels
+				+ ")" );
 
-		final WaveFileWriter waveWriter = new WaveFileWriter( "/tmp/javalibsndfilereader.wave",
-				2,
-				sampleRate,
-				(short)16 );
+		final WaveFileWriter waveWriter = new WaveFileWriter( "/tmp/javalibsndfilereader.wave", 2, sampleRate,
+				(short) 16 );
 
 		final int numFramesPerRound = BUFFER_LENGTH_FLOATS / numChannels;
 
@@ -69,44 +61,93 @@ public class AttemptToReadAFile
 
 		while( numFramesLeft > 0 )
 		{
-			final long numFramesThisRound = (numFramesLeft > numFramesPerRound ? numFramesPerRound : numFramesLeft );
+			final long numFramesThisRound = (numFramesLeft > numFramesPerRound ? numFramesPerRound : numFramesLeft);
 
-//			final long numFramesRead = libsndfile.sf_readf_float( sndfilePtr, floatPtr, numFramesThisRound );
-			final long numFramesRead = libsndfile.CustomSfReadfFloat( sndfilePtr, buffer, numFramesThisRound );
+			final long numFramesRead = libsndfile.sf_readf_float( sndfilePtr, floatPtr, numFramesThisRound );
 
-			assert( numFramesRead == numFramesThisRound );
+			assert (numFramesRead == numFramesThisRound);
 
-			final int numFloatsThisRound = (int)(numFramesThisRound * numChannels);
-//			for( int i = 0 ; i < numFloatsThisRound ; ++i )
-//			{
-//				buffer[i] = cArrayFloat.getitem( i );
-//			}
+			final int numFloatsThisRound = (int) (numFramesThisRound * numChannels);
+			for( int i = 0 ; i < numFloatsThisRound ; ++i )
+			{
+				buffer[i] = cArrayFloat.getitem( i );
+			}
 
 			waveWriter.writeFloats( buffer, numFloatsThisRound );
 
 			numFramesLeft -= numFramesThisRound;
+		}
+		libsndfile.sf_close( sndfilePtr );
 
-//			break;
+		waveWriter.close();
+
+		final long a = System.nanoTime();
+		final long diff = a - b;
+		log.trace( "Slow Done in " + diff + " nanos or " + (diff / 1000.0f) + " micros or " + (diff / 1000000.0f)
+				+ " millis" );
+	}
+
+	public void goBulkMethod() throws Exception
+	{
+
+		final SF_INFO sf = new SF_INFO();
+
+		log.trace( "Beginning" );
+
+		final SWIGTYPE_p_SNDFILE_tag sndfilePtr = libsndfile.sf_open( filePath, libsndfile.SFM_READ, sf );
+
+		if( sndfilePtr == null )
+		{
+			throw new DatastoreException( "Unknown format (returned null)" );
 		}
 
+		final int sampleRate = sf.getSamplerate();
+		final long numFrames = sf.getFrames();
+		final int numChannels = sf.getChannels();
+		log.trace( "Apparently have opened the file with (" + sampleRate + ") (" + numFrames + ") (" + numChannels
+				+ ")" );
+
+		final WaveFileWriter waveWriter = new WaveFileWriter( "/tmp/javalibsndfilereader.wave", 2, sampleRate,
+				(short) 16 );
+
+		final int numFramesPerRound = BUFFER_LENGTH_FLOATS / numChannels;
+
+		long numFramesLeft = numFrames;
+
+		final long b = System.nanoTime();
+
+		while( numFramesLeft > 0 )
+		{
+			final long numFramesThisRound = (numFramesLeft > numFramesPerRound ? numFramesPerRound : numFramesLeft);
+
+			final long numFramesRead = libsndfile.CustomSfReadfFloat( sndfilePtr, buffer, numFramesThisRound );
+
+			assert (numFramesRead == numFramesThisRound);
+
+			final int numFloatsThisRound = (int) (numFramesThisRound * numChannels);
+
+			waveWriter.writeFloats( buffer, numFloatsThisRound );
+
+			numFramesLeft -= numFramesThisRound;
+		}
 
 		libsndfile.sf_close( sndfilePtr );
 
 		waveWriter.close();
 
 		final long a = System.nanoTime();
-		final long diff = a-b;
-		log.trace( "Done in " + diff + " nanos or " + (diff/1000.0f) + " micros or " + (diff/1000000.0f) + " millis");
-
-		log.trace( "Done" );
+		final long diff = a - b;
+		log.trace( "Bulk done in " + diff + " nanos or " + (diff / 1000.0f) + " micros or " + (diff / 1000000.0f)
+				+ " millis" );
 	}
 
 	public static void main( final String[] args ) throws Exception
 	{
-		log.trace("Beginning libsndfile read attempt");
+		log.trace( "Beginning libsndfile read attempt" );
 		final AttemptToReadAFile a = new AttemptToReadAFile();
 
-		a.go();
+		a.goSlowMethod();
+		a.goBulkMethod();
 	}
 
 }
