0fdc0cedca93bbe47e63751be2296d289d513496
Daniel Hams <daniel.hams@gmail.com>
2015-12-03 15:54:37 +0000

    NumBitsEvaluator: Fix up the estimation of the number of significant bits to ignore boundaries, as that's where devices may fudge values so they cover and entire range.

--- 3UTIL/util-audio-test/src/test/uk/co/modularaudio/util/audio/math/NumBitsInControllerTester.java
+++ 3UTIL/util-audio-test/src/test/uk/co/modularaudio/util/audio/math/NumBitsInControllerTester.java
@@ -8,42 +8,78 @@ import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 import uk.co.modularaudio.util.audio.math.NumBitsEvaluator;
+import uk.co.modularaudio.util.tuple.TwoTuple;
 
 public class NumBitsInControllerTester
 {
 	private static Log log = LogFactory.getLog( NumBitsInControllerTester.class.getName() );
 
+//	private static final int NUM_TEST_VALUES = 60;
 	private static final int NUM_TEST_VALUES = 60 * 2;
+//	private static final int NUM_TEST_VALUES = 1024;
+//	private static final int NUM_TEST_VALUES = 8192;
 //	private static final int NUM_TEST_VALUES = 8192 * 10;
 
+//	private static final int NUM_BITS = 4;
+//	private static final int NUM_BITS = 6;
+//	private static final int NUM_BITS = 7;
+//	private static final int NUM_BITS = 10;
 	private static final int NUM_BITS = 14;
+//	private static final int NUM_BITS = 16;
+//	private static final int NUM_BITS = 20;
+//	private static final int NUM_BITS = 32;
 
-	private long[] generateValuesWithLimitedBits( final int numBits )
+//	private static final int NUM_SIGNIFICANT_BITS = 4;
+	private static final int NUM_SIGNIFICANT_BITS = 7;
+//	private static final int NUM_SIGNIFICANT_BITS = 10;
+//	private static final int NUM_SIGNIFICANT_BITS = 14;
+//	private static final int NUM_SIGNIFICANT_BITS = 20;
+//	private static final int NUM_SIGNIFICANT_BITS = 32;
+
+	private long[] generateValuesWithLimitedBits( final int totalBits, final int significantBits )
+		throws IndexOutOfBoundsException
 	{
+		final int numShiftBits = totalBits - significantBits;
+		if( totalBits > 63 )
+		{
+			throw new IndexOutOfBoundsException( "Cannot do more than 63 bits" );
+		}
 		final BigInteger bi = BigInteger.valueOf( 2 );
-		final BigInteger bMaxIntForBits = bi.pow(  numBits );
-		final long maxIntForBits = bMaxIntForBits.longValue() - 1;
-		final int numToShift = 63 - numBits;
+		final BigInteger bMaxSigIntForBits = bi.pow( significantBits );
+		final long maxSigIntForBits = bMaxSigIntForBits.longValue() - 1;
+
+		final BigInteger bMaxTotIntForBits = bi.pow( totalBits );
+		final long maxTotIntForBits = bMaxTotIntForBits.longValue() - 1;
+
 		final Random r = new Random();
 		final long[] retVal = new long[ NUM_TEST_VALUES ];
 
-		for( int i = 0 ; i < NUM_TEST_VALUES ; ++i )
+		int writePos = 0;
+
+		while( writePos < NUM_TEST_VALUES )
 		{
 			// Generate random normalised val between 0 and 1
 			// then quantise to number of required bits
 			// and scale back to normalised float
 			final double randVal = r.nextDouble();
+//			log.trace("Using rand of " + randVal );
 
-			final double scaledValue = randVal * maxIntForBits;
+			final double scaledValue = randVal * maxSigIntForBits;
 
 			//final long asLong = Math.round(scaledValue);
-			final long asLong = (long)scaledValue;
+			final long asLong = (long)(scaledValue + 0.5);
 
-			// And shift to fill the signed space of long
-
-			final long shiftedLong = asLong << numToShift;
-
-			retVal[i] = shiftedLong;
+			if( asLong == maxSigIntForBits )
+			{
+				// Simulate devices fudging how many bits they send by setting
+				// the last entry (1.0) to fully set bits for the entire bit range
+				retVal[writePos++] = maxTotIntForBits;
+			}
+			else
+			{
+				final long shiftedLong = asLong << numShiftBits;
+				retVal[writePos++] = shiftedLong;
+			}
 		}
 
 		return retVal;
@@ -54,9 +90,9 @@ public class NumBitsInControllerTester
 	{
 		log.debug("Doing it!");
 
-		final long[] sourceValuesToEvaluate = generateValuesWithLimitedBits( NUM_BITS );
+		final long[] sourceValuesToEvaluate = generateValuesWithLimitedBits( NUM_BITS, NUM_SIGNIFICANT_BITS );
 
-		final NumBitsEvaluator numBitsEvaluator = new NumBitsEvaluator();
+		final NumBitsEvaluator numBitsEvaluator = new NumBitsEvaluator( NUM_BITS );
 		int numValuesSoFar = 0;
 
 		for( final long sv : sourceValuesToEvaluate )
@@ -66,27 +102,30 @@ public class NumBitsInControllerTester
 			numValuesSoFar++;
 		}
 
-		numBitsEvaluator.dumpNFirstUniqueValues( 10 );
+		numBitsEvaluator.dumpNFirstUniqueValues( 100 );
 
-		final int numBitsEstimate = numBitsEvaluator.getNumBitsEstimate();
+		final TwoTuple<Integer,Float> numBitsAndConfidence = numBitsEvaluator.getNumBitsAndConfidence();
+		final int numBits = numBitsAndConfidence.getHead();
+		final float confidence = numBitsAndConfidence.getTail();
 
-		if( numBitsEstimate != -1 )
+		if( confidence > 0.0f )
 		{
 			log.info( "Have enough values to make num bits estimate" );
-			log.info( "After " + numValuesSoFar + " values num bits estimate is " + numBitsEstimate );
+			log.info( "After " + numValuesSoFar + " values num bits estimate is " + numBits +
+					" with a confidence of " + confidence );
 			final BigInteger bi = BigInteger.valueOf( 2 );
-			final long numValuesForBits = bi.pow( numBitsEstimate ).longValue();
+			final long numValuesForBits = bi.pow( numBits ).longValue();
 			log.info( "This is " + numValuesForBits + " unique values");
 		}
 		else
 		{
-			log.info( "Failed to estimate number of bits" );
+			log.info( "Failed to estimate number of bits." );
 		}
 
 //		log.info( "About to do 1000 calls" );
 //		for( int i = 0 ; i < 1000 ; ++i )
 //		{
-//			numBitsEstimate = numBitsEvaluator.getNumBitsEstimate();
+//			numBitsAndConfidence = numBitsEvaluator.getNumBitsAndConfidence();
 //		}
 //		log.info( "Done 1000 calls" );
 	}
--- 3UTIL/util-audio/src/uk/co/modularaudio/util/audio/math/NumBitsEvaluator.java
+++ 3UTIL/util-audio/src/uk/co/modularaudio/util/audio/math/NumBitsEvaluator.java
@@ -1,54 +1,99 @@
 package uk.co.modularaudio.util.audio.math;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.mahout.math.list.LongArrayList;
-import org.apache.mahout.math.map.OpenLongLongHashMap;
+import org.apache.mahout.math.map.OpenLongIntHashMap;
 import org.apache.mahout.math.set.OpenLongHashSet;
 
 import uk.co.modularaudio.util.math.FastMath;
+import uk.co.modularaudio.util.tuple.TwoTuple;
 
 public class NumBitsEvaluator
 {
 	private static Log log = LogFactory.getLog( NumBitsEvaluator.class.getName() );
 
 	// Map from int bits to num
-	private final OpenLongLongHashMap valueHashMap = new OpenLongLongHashMap();
+	private final OpenLongIntHashMap valueHashMap = new OpenLongIntHashMap();
+
+	private int numSignificantBits;
+
+	public NumBitsEvaluator( final int numSignificantBits )
+	{
+		reset( numSignificantBits );
+	}
+
+	public final void reset( final int numSignificantBits )
+	{
+		this.numSignificantBits = numSignificantBits;
+	}
 
 	public void addValue( final long sv )
 	{
 		if( valueHashMap.containsKey( sv ) )
 		{
-			final long num = valueHashMap.get( sv );
+			final int num = valueHashMap.get( sv );
 			valueHashMap.put( sv, num+1 );
 		}
 		else
 		{
-			valueHashMap.put( sv, 1L );
+			valueHashMap.put( sv, 1 );
 		}
 	}
 
-	public int getNumBitsEstimate()
+	public TwoTuple<Integer,Float> getNumBitsAndConfidence()
 	{
 		if( valueHashMap.size() < 10 )
 		{
 			// Not enough values yet
-			return -1;
+			return new TwoTuple<>( -1, 0.0f );
 		}
 		// Work out what the min delta between values is and check that
 		// all deltas are a multiple of that min value
+		final BigInteger bi = BigInteger.valueOf( 2 );
+		final BigInteger bMaxIntForBits = bi.pow(  numSignificantBits );
+		final long maxIntForBits = bMaxIntForBits.longValue() - 1;
+
+		// First eliminate the zeroth and fully set values from having deltas
+		// computed to get around fudging done by equipment without
+		// the full bit range
 		final LongArrayList keysArrayList = valueHashMap.keys();
-		final long[] keys = keysArrayList.elements();
+		final int numOrigKeys = keysArrayList.size();
+//		log.trace( "NumOrigKeys=" + numOrigKeys );
+		final LongArrayList noBoundariesKeysList = new LongArrayList();
+		for( int i = 0 ; i < numOrigKeys ; ++i )
+		{
+			final long oneKey = keysArrayList.get( i );
+			if( oneKey != 0 &&
+					oneKey != maxIntForBits)
+			{
+				noBoundariesKeysList.add( oneKey );
+			}
+			else
+			{
+				log.trace( "Eliminated boundary value " + oneKey );
+				log.trace( "This is bitset " + longToBitsetString( oneKey ) );
+			}
+		}
+		final int numNoBoundariesKeys = noBoundariesKeysList.size();
+//		log.trace( "NumNoBoundariesKeys=" + numNoBoundariesKeys );
+		if( numNoBoundariesKeys != numOrigKeys )
+		{
+			log.trace( "Eliminated boundary value(s)" );
+		}
+
+		final long[] noBoundariesKeys = noBoundariesKeysList.toArray( new long[numNoBoundariesKeys] );
 
 //		log.trace( "Sorting values" );
 
-		final long[] sortedVals = new long[keys.length];
-		System.arraycopy( keys, 0, sortedVals, 0, keys.length );
+		final long[] sortedVals = new long[numNoBoundariesKeys];
+		System.arraycopy( noBoundariesKeys, 0, sortedVals, 0, numNoBoundariesKeys );
 		Arrays.sort( sortedVals );
 
-		final int numDeltas = keys.length - 1;
+		final int numDeltas = numNoBoundariesKeys - 1;
 
 		final OpenLongHashSet uniqDeltas = new OpenLongHashSet( numDeltas );
 		long minDelta = Long.MAX_VALUE;
@@ -70,31 +115,61 @@ public class NumBitsEvaluator
 
 		final int numUniqDeltas = uniqDeltas.size();
 
-//		log.trace( "Found " + numUniqDeltas + " unique deltas" );
+		final LongArrayList uniqDeltaArrayList = uniqDeltas.keys();
+		final long[] uniqDeltaArray = uniqDeltaArrayList.elements();
 
-		final double numUniqValsForMinDeltaDouble = ((double)Long.MAX_VALUE) / minDelta;
-		final long numUniqValsForMinDelta = (long)numUniqValsForMinDeltaDouble;
+//		log.trace( "Found " + numUniqDeltas + " unique deltas minDelta=" + minDelta );
+//		for( int i = 0 ; i < numUniqDeltas ; ++i )
+//		{
+//			log.trace( "UniqueDelta[" + i + "]=" + uniqDeltaArray[i] );
+//		}
+
+		double numUniqValsForMinDeltaDouble = ((double)maxIntForBits) / minDelta;
+		long numUniqValsForMinDelta = (long)numUniqValsForMinDeltaDouble;
 
 		// Use a tenth of the min delta as the error bound for other deltas being a multiple
 		final double maxError = 0.1;
 
 		boolean deltasConform = true;
 
-		final LongArrayList uniqDeltaArrayList = uniqDeltas.keys();
-		final long[] uniqDeltaArray = uniqDeltaArrayList.elements();
-
 //		log.trace( "Testing delta conformity" );
+
+		double curMinDelta = minDelta;
+		float confidence = 1.0f;
+		int divisor = 2;
 
 		for( int d = 1 ; d < numUniqDeltas ; ++d )
 		{
 			final long deltaToTest = uniqDeltaArray[d];
-			final double numMinDeltas = ((double)deltaToTest) / minDelta;
 
-			final double remainder = numMinDeltas % 1;
-			if( remainder > maxError )
+			double numMinDeltas = (deltaToTest) / curMinDelta;
+			double remainder = numMinDeltas % 1;
+			do
 			{
-				deltasConform = false;
+				if( remainder > maxError )
+				{
+					if( divisor < 8 )
+					{
+						curMinDelta = minDelta / divisor;
+						divisor++;
+						confidence = confidence / 2;
+					}
+					else
+					{
+						deltasConform = false;
+						break;
+					}
+				}
+				else
+				{
+					break;
+				}
+				numMinDeltas = (deltaToTest) / curMinDelta;
+				remainder = numMinDeltas % 1;
+				numUniqValsForMinDeltaDouble = ((double)maxIntForBits) / minDelta;
+				numUniqValsForMinDelta = (long)numUniqValsForMinDeltaDouble;
 			}
+			while( remainder > maxError );
 		}
 //		log.trace( "Calculating num bits" );
 
@@ -107,11 +182,11 @@ public class NumBitsEvaluator
 
 		if( !deltasConform )
 		{
-			return -1;
+			return new TwoTuple<>( -1, 0.0f );
 		}
 		else
 		{
-			return numBits;
+			return new TwoTuple<>( numBits, confidence );
 		}
 	}
 
@@ -121,11 +196,24 @@ public class NumBitsEvaluator
 		final long[] sortedVals = new long[sourceVals.length];
 		System.arraycopy( sourceVals, 0, sortedVals, 0, sourceVals.length );
 		Arrays.sort( sortedVals );
-		for( int i = 0 ; i < n ; ++i )
+		for( int i = 0 ; i < n && i < sourceVals.length ; ++i )
 		{
 			final long k = sortedVals[i];
 			final long numForVal = valueHashMap.get( k );
 			log.debug("Have " + numForVal + " occurences of value " + k );
+
+			log.debug("This is bitset " + longToBitsetString( k ) );
 		}
+	}
+
+	private String longToBitsetString( final long v )
+	{
+		final StringBuilder sb = new StringBuilder();
+		for( int b = 0 ; b < 64 ; ++b )
+		{
+			final long testVal = (0x01L << (63-b));
+			sb.append( (v & testVal) == 0 ? '0' : '1' );
+		}
+		return sb.toString();
 	}
 }
